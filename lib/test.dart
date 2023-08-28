6628fe97a8636aa93f700786c125f670922457df
nguyenngocsonn19 <93904808+nguyenngocsonn19@users.noreply.github.com>
2023-08-28 15:33:04 +0700

    submit

--- lib/test.dart
+++ lib/test.dart
@@ -28,6 +28,7 @@ class MusicController {
   }
 
   Music getMusic(int index) {
+
     if (index >= 0 && index < musicList.length) {
       return musicList[index];
     }
@@ -50,65 +51,89 @@ void main() {
     print("5. Exit");
 
     var choice = int.parse(stdin.readLineSync()!);
+    try {
+      switch (choice) {
+        case 1:
+          print("Enter music title:");
+          var title = stdin.readLineSync();
+          print("Enter artist:");
+          var artist = stdin.readLineSync();
+          print("Enter duration (seconds):");
+          var duration = int.parse(stdin.readLineSync()!);
 
-    switch (choice) {
-      case 1:
-        print("Enter music title:");
-        var title = stdin.readLineSync();
-        print("Enter artist:");
-        var artist = stdin.readLineSync();
-        print("Enter duration (seconds):");
-        var duration = int.parse(stdin.readLineSync()!);
 
-        var newMusic = Music(title!, artist!, duration);
-        musicController.addMusic(newMusic);
-        print("Music added");
-        break;
+          var newMusic = Music(title!, artist!, duration);
 
-      case 2:
-        print("Enter index of music to update:");
-        var index = int.parse(stdin.readLineSync()!);
-        if (index >= 0 && index < musicController.musicList.length) {
-          print("Enter new music title:");
-          var newTitle = stdin.readLineSync();
-          print("Enter new artist:");
-          var newArtist = stdin.readLineSync();
-          print("Enter new duration (seconds):");
-          var newDuration = int.parse(stdin.readLineSync()!);
+          musicController.addMusic(newMusic);
+          print("Music added");
 
-          var updatedMusic = Music(newTitle!, newArtist!, newDuration);
-          musicController.updateMusic(index, updatedMusic);
-          print("Music updated");
-        } else {
-          print("Invalid index");
-        }
-        break;
+          break;
 
-      case 3:
-        print("Enter index of music to delete:");
-        var index = int.parse(stdin.readLineSync()!);
-        if (index >= 0 && index < musicController.musicList.length) {
-          musicController.deleteMusic(index);
-          print("Music deleted");
-        } else {
-          print("Invalid index");
-        }
-        break;
+        case 2:
+          if (musicController.musicList.isEmpty) {
+            print("Music is empty");
+          } else {
+            print("Enter index of music to update:");
+            var index = int.parse(stdin.readLineSync()!);
+            if (index >= 0 && index < musicController.musicList.length) {
+              print("Enter new music title:");
+              var newTitle = stdin.readLineSync();
+              print("Enter new artist:");
+              var newArtist = stdin.readLineSync();
+              print("Enter new duration (seconds):");
+              var newDuration = int.parse(stdin.readLineSync()!);
 
-      case 4:
-        print("All Music:");
-        for (var index = 0; index < musicController.musicList.length; index++) {
-          var music = musicController.getMusic(index);
-          print("$index. ${music.title} by ${music.artist} (${music.duration} seconds)");
-        }
-        break;
+              var updatedMusic = Music(newTitle!, newArtist!, newDuration);
+              musicController.updateMusic(index, updatedMusic);
+              print("Music updated");
+            } else {
+              print("Invalid index");
+            }
+          }
 
-      case 5:
-        print("Exit program");
-        return;
+          break;
 
-      default:
-        print("Choose not found");
+        case 3:
+          if (musicController.musicList.isEmpty) {
+            print("Music is empty");
+            break;
+          }
+            print("Enter index of music to delete:");
+            var index = int.parse(stdin.readLineSync()!);
+            if (index >= 0 && index < musicController.musicList.length) {
+              musicController.deleteMusic(index);
+              print("Music deleted");
+            } else {
+              print("Invalid index");
+            }
+
+
+          break;
+
+        case 4:
+          if (musicController.musicList.isEmpty) {
+            print("Music is empty");
+          } else {
+            print("All Music:");
+            for (var index = 0; index <
+                musicController.musicList.length; index++) {
+              var music = musicController.getMusic(index);
+
+              print("$index. ${music.title} by ${music.artist} (${music
+                  .duration} seconds)");
+            }
+          }
+          break;
+
+        case 5:
+          print("Exit program");
+          return;
+
+        default:
+          print("Choose not found");
+      }
+    }catch(e){
+      print("Please enter init");
     }
   }
 }
